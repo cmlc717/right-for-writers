3b42a3fd8bb227e48cc49c30874eb1419b3213a8
Carina Carlos <121907876+cmlc717@users.noreply.github.com>
2023-07-27 23:09:46 -0400

    more relations

--- server/db/index.js
+++ server/db/index.js
@@ -8,8 +8,21 @@ const Comment = require('../models/comment');
 
 Post.belongsTo(User, {as: "Author", foreignKey: "AuthorId"});
 Post.belongsTo(User, {as: "Editor", foreignKey: "EditorId"});
+Post.belongsToMany(User, {through: "user-likes"});
+Post.belongsToMany(User, {through: "user-favorites"});
+Post.belongsToMany(User, {through: "user-bookmarks"});
+Post.belongsToMany(Tag, {through: "post-tag"});
+Post.belongsToMany(Genre, {through: "post-genre"});
+Post.hasMany(Comment);
+
 User.hasMany(Post, {as: "Author"});
 User.hasMany(Post, {as: "Editor"});
+User.hasMany(Comment);
+
+Tag.belongsToMany(Post, {through: "post-tag"});
+Genre.belongsToMany(Post, {through: "post-genre"});
+Comment.belongsTo(Post);
+Comment.belongsTo(User);
 
 module.exports = {
     db,
